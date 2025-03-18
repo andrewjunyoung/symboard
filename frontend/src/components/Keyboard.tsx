9081e31b6c6bec3fc89439b0eb6655003cf99a51
ajy <andrew@getinstant.app>
2025-03-18 00:21:49 -0400

    Keyboard loaded from keylayout file.

--- frontend/src/components/Keyboard.tsx
+++ frontend/src/components/Keyboard.tsx
@@ -1,11 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Keyboard.css";
 
 interface KeyProps {
   children: React.ReactNode;
-  width?: number; // Width in units (1 unit = standard key width)
-  height?: number; // Height in units
+  width?: number;
+  height?: number;
   className?: string;
+}
+
+export async function loadKeylayoutFile(filePath) {
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error(`Failed to load file: ${response.statusText}`);
+    }
+
+    const xmlText = await response.text();
+
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+
+    if (xmlDoc.getElementsByTagName("parsererror").length > 0) {
+      throw new Error("Error parsing XML");
+    }
+
+    const keylayoutObject = parseKeylayoutXML(xmlDoc);
+
+    return keylayoutObject;
+  } catch (error) {
+    console.error("Error loading keylayout file:", error);
+    return null;
+  }
+}
+
+function parseKeylayoutXML(xmlDoc) {
+  const keylayout = {
+    layouts: [],
+    keyMapSets: {},
+    keyMaps: {},
+    actions: {},
+    modifiers: {},
+  };
+
+  const keyboard = xmlDoc.getElementsByTagName("keyboard")[0];
+  if (keyboard) {
+    keylayout.name = keyboard.getAttribute("name");
+    keylayout.group = keyboard.getAttribute("group");
+    keylayout.id = keyboard.getAttribute("id");
+  }
+
+  const modifierMaps = xmlDoc.getElementsByTagName("modifierMap");
+  for (let map of modifierMaps) {
+    const id = map.getAttribute("id");
+    keylayout.modifiers[id] = [];
+    const keys = map.getElementsByTagName("key");
+    for (let key of keys) {
+      keylayout.modifiers[id].push({
+        code: key.getAttribute("code"),
+        modifiers: key.getAttribute("modifiers"),
+      });
+    }
+  }
+
+  const keyMapSets = xmlDoc.getElementsByTagName("keyMapSet");
+  for (let set of keyMapSets) {
+    const id = set.getAttribute("id");
+    keylayout.keyMapSets[id] = [];
+    const keyMaps = set.getElementsByTagName("keyMap");
+    for (let map of keyMaps) {
+      const index = map.getAttribute("index");
+      keylayout.keyMapSets[id].push(index);
+      keylayout.keyMaps[index] = {};
+      const keys = map.getElementsByTagName("key");
+      for (let key of keys) {
+        keylayout.keyMaps[index][key.getAttribute("code")] = {
+          output: key.getAttribute("output"),
+          action: key.getAttribute("action"),
+        };
+      }
+    }
+  }
+
+  const actions = xmlDoc.getElementsByTagName("action");
+  for (let action of actions) {
+    const id = action.getAttribute("id");
+    keylayout.actions[id] = [];
+    const whens = action.getElementsByTagName("when");
+    for (let when of whens) {
+      keylayout.actions[id].push({
+        state: when.getAttribute("state"),
+        output: when.getAttribute("output"),
+      });
+    }
+  }
+
+  return keylayout;
 }
 
 const Key: React.FC<KeyProps> = ({
@@ -28,6 +117,23 @@ const Key: React.FC<KeyProps> = ({
 };
 
 const Keyboard: React.FC = () => {
+  const [keylayout, setKeylayout] = useState(null);
+
+  useEffect(() => {
+    const fetchKeylayout = async () => {
+      const filePath = "/layout.keylayout";
+      const result = await loadKeylayoutFile(filePath);
+
+      if (result) {
+        console.log("Keylayout Object:", result);
+        console.log("Key map for index 0:", result.keyMaps["0"]["0"].output);
+        setKeylayout(result);
+      }
+    };
+
+    fetchKeylayout();
+  }, []);
+
   return (
     <div className="keyboard-container">
       <div className="keyboard">
@@ -53,44 +159,30 @@ const Keyboard: React.FC = () => {
 
         {/* Number row */}
         <div className="keyboard-row">
-          <Key>
-            ~<br />`
+          <Key>{keylayout ? keylayout.keyMaps["0"]["50"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["89"].output : "X"}
           </Key>
-          <Key>
-            !<br />1
+          <Key>{keylayout ? keylayout.keyMaps["0"]["91"].output : "X"}
           </Key>
-          <Key>
-            @<br />2
+          <Key>{keylayout ? keylayout.keyMaps["0"]["92"].output : "X"}
           </Key>
-          <Key>
-            #<br />3
+          <Key>{keylayout ? keylayout.keyMaps["0"]["86"].output : "X"}
           </Key>
-          <Key>
-            $<br />4
+          <Key>{keylayout ? keylayout.keyMaps["0"]["87"].output : "X"}
           </Key>
-          <Key>
-            %<br />5
+          <Key>{keylayout ? keylayout.keyMaps["0"]["88"].output : "X"}
           </Key>
-          <Key>
-            ^<br />6
+          <Key>{keylayout ? keylayout.keyMaps["0"]["83"].output : "X"}
           </Key>
-          <Key>
-            &<br />7
+          <Key>{keylayout ? keylayout.keyMaps["0"]["84"].output : "X"}
           </Key>
-          <Key>
-            *<br />8
+          <Key>{keylayout ? keylayout.keyMaps["0"]["85"].output : "X"}
           </Key>
-          <Key>
-            (<br />9
+          <Key>{keylayout ? keylayout.keyMaps["0"]["82"].output : "X"}
           </Key>
-          <Key>
-            )<br />0
+          <Key>{keylayout ? keylayout.keyMaps["0"]["0"].output : "X"}
           </Key>
-          <Key>
-            _<br />-
-          </Key>
-          <Key>
-            +<br />=
+          <Key>{keylayout ? keylayout.keyMaps["0"]["0"].output : "X"}
           </Key>
           <Key width={2}>Backspace</Key>
         </div>
@@ -98,28 +190,20 @@ const Keyboard: React.FC = () => {
         {/* QWERTY row */}
         <div className="keyboard-row">
           <Key width={1.5}>Tab</Key>
-          <Key>Q</Key>
-          <Key>W</Key>
-          <Key>E</Key>
-          <Key>R</Key>
-          <Key>T</Key>
-          <Key>Y</Key>
-          <Key>U</Key>
-          <Key>I</Key>
-          <Key>O</Key>
-          <Key>P</Key>
-          <Key>
-            {"{"}
-            <br />
-            {"["}
-          </Key>
-          <Key>
-            {"}"}
-            <br />
-            {"]"}
-          </Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["0"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["13"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["14"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["15"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["16"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["17"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["32"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["34"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["31"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["35"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["33"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["30"].output : "X"}</Key>
           <Key width={1.5}>
-            |<br />\
+            {keylayout ? keylayout.keyMaps["0"]["42"].output : "X"}
           </Key>
         </div>
 
@@ -128,22 +212,18 @@ const Keyboard: React.FC = () => {
           <Key width={1.75} className="mod-key">
             Caps Lock
           </Key>
-          <Key>A</Key>
-          <Key>S</Key>
-          <Key>D</Key>
-          <Key>F</Key>
-          <Key>G</Key>
-          <Key>H</Key>
-          <Key>J</Key>
-          <Key>K</Key>
-          <Key>L</Key>
-          <Key>
-            :<br />;
-          </Key>
-          <Key>
-            "<br />'
-          </Key>
-          <Key width={2.25} className="mod-key">
+          <Key>{keylayout ? keylayout.keyMaps["0"]["0"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["1"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["2"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["3"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["5"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["4"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["38"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["40"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["37"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["41"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["39"].output : "X"}</Key>
+          <Key width={2.4} className="mod-key">
             Enter
           </Key>
         </div>
@@ -153,32 +233,24 @@ const Keyboard: React.FC = () => {
           <Key width={2.25} className="mod-key">
             Shift
           </Key>
-          <Key>Z</Key>
-          <Key>X</Key>
-          <Key>C</Key>
-          <Key>V</Key>
-          <Key>B</Key>
-          <Key>N</Key>
-          <Key>M</Key>
-          <Key>
-            {"<"}
-            <br />,
-          </Key>
-          <Key>
-            {">"}
-            <br />.
-          </Key>
-          <Key>
-            ?<br />/
-          </Key>
-          <Key width={2.75} className="mod-key">
+          <Key>{keylayout ? keylayout.keyMaps["0"]["6"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["7"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["8"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["9"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["11"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["45"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["46"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["43"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["47"].output : "X"}</Key>
+          <Key>{keylayout ? keylayout.keyMaps["0"]["44"].output : "X"}</Key>
+          <Key width={3} className="mod-key">
             Shift
           </Key>
         </div>
 
         {/* Bottom row */}
         <div className="keyboard-row">
-          <Key width={1.25} className="mod-key">
+          <Key width={1.5} className="mod-key">
             Ctrl
           </Key>
           <Key width={1.25} className="mod-key">
@@ -197,7 +269,7 @@ const Keyboard: React.FC = () => {
           <Key width={1.25} className="mod-key">
             Menu
           </Key>
-          <Key width={1.25} className="mod-key">
+          <Key width={1.6} className="mod-key">
             Ctrl
           </Key>
         </div>
